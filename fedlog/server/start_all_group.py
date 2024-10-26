9aaffd5322a8dd74e4db6ee49ebb6c6d25a6d32a
yzzer <yzzerchina@gmail.com>
2024-10-26 21:49:51 +0800

    bugfix

--- fedlog/server/start_all_group.py
+++ fedlog/server/start_all_group.py
@@ -7,6 +7,7 @@ class Config(BaseModel):
     log_dir: str = "logs"
     client_num: int = 10
     mode: str = "start"  # start / stop
+    part: str = "client"
     
 
 
@@ -21,11 +22,13 @@ def get_config() -> Config:
     parser.add_argument("--log-dir", default="logs", required=False)
     parser.add_argument("--client-num", type=int, default=3, required=False)
     parser.add_argument("--mode", required=True)
+    parser.add_argument("--part", required=False)
+    
 
     # 解析命令行参数
     args = parser.parse_args()
     
-    return Config(config_path=args.config, log_dir=args.log_dir, client_num=args.client_num, mode=args.mode)
+    return Config(config_path=args.config, log_dir=args.log_dir, client_num=args.client_num, mode=args.mode, part=args.part)
 
 
 def start_client_nohup(config_path: str, log_dir: str, client_id: int):
@@ -58,13 +61,14 @@ def main():
         os.makedirs(conf.log_dir)
         
         for i in range(conf.client_num):
-            pid = start_client_nohup(conf.config_path, conf.log_dir, i)
-            conf.pids.append(pid)
-            print(f"start client {i} pid: {pid}")
-            
-            pid = start_trainer_nohup(conf.config_path, conf.log_dir, i)
-            conf.pids.append(pid)
-            print(f"start trainer {i} pid: {pid}")
+            if conf.part == "client":
+                pid = start_client_nohup(conf.config_path, conf.log_dir, i)
+                conf.pids.append(pid)
+                print(f"start client {i} pid: {pid}")
+            else:
+                pid = start_trainer_nohup(conf.config_path, conf.log_dir, i)
+                conf.pids.append(pid)
+                print(f"start trainer {i} pid: {pid}")
         json.dump(conf.model_dump(), open("./cache.json", "w"))
     else:
         json_conf = json.load(open("./cache.json", "r"))
